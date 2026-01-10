415ca570f33af0d2747b6428611bfa2fdbd30143
Keshav <kk2556583@gmail.com>
2026-01-10 22:01:09 +0530

    optimized way

--- book allocation.cpp
+++ book allocation.cpp
@@ -45,7 +45,7 @@ int bookallocation(int arr[] , int n,int k){
 	   
 }}
 int main() {
-	int arr[]={12,34,67,98};
+	int arr[]={12,34,67,87};
 	int n = 4 ;
 	int k = 2;
 	cout<<bookallocation(arr,n,k); // output - 113 
