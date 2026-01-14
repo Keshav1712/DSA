3315470f3a1acf2670994fb0432dcb9de66ae795
Keshav <kk2556583@gmail.com>
2026-01-15 01:36:04 +0530

    Pair difference problem using two pointer

--- pairdiff2poiner.cpp
+++ pairdiff2poiner.cpp
@@ -1,30 +1,40 @@
 #include<iostream>
 using namespace std;
+/*  
+pair difference problem   find the pair which diff is target vlaue 
+1st apporach - brute force using 2 loops 
+2nd pproach - o(nlog) using binary search 
+3rd approach - two pointer o(n) -- but need sorted array 
 
+*/
+
+
+ // NEED sorted array 
 void diff(int arr[],int n , int target ){
 	int start =0;
 	int end =1 ;
 	
-	while (start<end){
+	while (end<n){
 		int dif = arr[end]-arr[start];
 		
 		if(dif==target){
-			cout<<arr[end]<<arr[start]<<endl;
+			cout<<"pairs  are : "<<arr[end]<<" and  "<<arr[start]<<endl;
+			break;
 		}
 		else if (dif>target){
 			start++;
 		}
 		else {
-			end--;
+			end++;
 		}
 	}
 }
 
 
 int main (){
-	int arr []={2,3,5,10,50,80};
+	int arr []={2,3,5,10,50,80}; // need sorted array 
 	int n = sizeof(arr)/sizeof(arr[0]);
 	int target =45;
-	diff(arr,n,target);
+	diff(arr,n,target);// output - 50 ,5 
 	return 0;
 }
