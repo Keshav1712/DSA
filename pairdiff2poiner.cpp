fe33d185aaca931030f80314247d6375981316a7
Keshav <kk2556583@gmail.com>
2026-01-17 18:50:37 +0530

    Updated code with sorted function and few edge cases handling

--- pairdiff2poiner.cpp
+++ pairdiff2poiner.cpp
@@ -9,32 +9,66 @@ pair difference problem   find the pair which diff is target vlaue
 */
 
 
- // NEED sorted array 
-void diff(int arr[],int n , int target ){
+ //sorted array 
+ void sorte(int arr[],int n){
+ 	for(int i =1;i<n;i++){
+		for(int j=i;j>0;j--){
+			if(arr[j]<arr[j-1]){
+				swap(arr[j],arr[j-1]);
+			}else{
+				break;
+			}
+		}
+	
+		
+ }
+ 	// for displaying array 
+ 	cout<<"sorted array is ";
+		for (int i=0;i<n;i++ ){
+			
+			cout<<" "<<arr[i];
+		}
+		cout<<" "<<endl;
+		}
+ 
+ 
+ 
+void diffe(int arr[],int n , int target ){
 	int start =0;
 	int end =1 ;
-	
+	bool found = false ;
 	while (end<n){
 		int dif = arr[end]-arr[start];
 		
 		if(dif==target){
 			cout<<"pairs  are : "<<arr[end]<<" and  "<<arr[start]<<endl;
+			found = true ;
 			break;
 		}
 		else if (dif>target){
 			start++;
 		}
-		else {
+		else{
 			end++;
 		}
+		
+		
 	}
-}
+	 if (!found) {
+        cout << "Pair is not available" << endl;
+	
+}}
 
 
 int main (){
-	int arr []={2,3,5,10,50,80}; // need sorted array 
+	int arr []={10,3,11,9,7}; // need sorted array 
 	int n = sizeof(arr)/sizeof(arr[0]);
-	int target =45;
-	diff(arr,n,target);// output - 50 ,5 
+	int target =5;  
+	int tr = 7; 
+	
+	sorte(arr,n);
+	diffe(arr,n,target);//not availble - output is "pairs not availble "
+	diffe (arr,n,tr); // output -"pairs  are : 10 and  3"
+	
 	return 0;
 }
