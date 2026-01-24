d56c80a2735d6c5edfb6b88b1088e713011e3d55
Keshav <kk2556583@gmail.com>
2026-01-24 22:15:13 +0530

    Update with fe comments

--- Maxdiff.cpp
+++ Maxdiff.cpp
@@ -1,11 +1,19 @@
 
 #include <iostream>
 using namespace std;
+/*
+This is a problem to fnd max differnece between two element in a given array 
+time complexity - o(n) 
+working - 
+There is two variable first one store minimum element of a array and another one is largest difference  
+
+*/
+
 int Maxdiff(int arr [], int n){
     int minelement = arr[0];
     int maxdifference = arr[1]-arr[0];
     for (int i=1; i<n;i++){
-        maxdifference =max(maxdifference,arr[i]-minelement);
+        maxdifference =max(maxdifference,arr[i]-minelement);// store max diff 
         minelement = min(minelement , arr[i]);
     }
     return maxdifference;
