689dad92ca634a5f1ff7d58989d73062d6ca52ef
Keshav <kk2556583@gmail.com>
2026-01-31 22:23:30 +0530

    2D array input and output

--- 2d array.cpp
+++ 2d array.cpp
@@ -1,12 +1,24 @@
-#include<iostream>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-int main(){
-	
-	int a [4][3];
-	
-	
-	
-	
-	return 0;
+int main() {
+    int a[4][3];
+
+    // input
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // output
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
+
