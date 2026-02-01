e3a7f46b6108610f764cf3443482557cc86fbc0e
Keshav <kk2556583@gmail.com>
2026-02-01 23:26:01 +0530

    Search element in 2d array

---
+++ Search element in 2d array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+int arr [4][3]={1,2,3,4,5,6,7,8,9,10,11,12};
+int x = 4;
+
+for(int i =0;i<4;i++){
+	for (int j=0;j<3;j++){
+		if(arr[i][j]==x){
+			cout << " found at index : "<<i<<" "<<j<<endl;
+			return 0;
+		}
+	}
+}
+cout << "not found "<<endl;
+
+
+
+
+	return 0;
+}
