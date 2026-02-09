d6a6e3430480b7507e7a2771e81349286b46559f
Keshav <kk2556583@gmail.com>
2026-02-09 23:18:22 +0530

    Reverse of 2D array Row wise

---
+++ revese of 2d array colm wise.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std ;
+/*
+ Reverse of a 2d array row wise 
+ time complexity - o(ncube)
+
+
+*/
+int main(){
+	
+	int arr [3][3]={1,2,3,4,5,6,7,8,9};
+	int col = 3 , row = 3;
+	
+	for(int i =0;i<row;i++){
+		int start=0,end = col-1;
+		while (start <end ){
+			swap(arr[i][start],arr[i][end]);
+			start ++;
+			end --;
+			
+		}
+		
+	}
+	
+	for (int i =0;i<row;i++){
+		for (int j =0;j<col;j++){
+			cout<<arr[i][j]<<"\t";
+		}
+		 cout << endl;  
+	}
+	
+	
+	return 0;
+}
