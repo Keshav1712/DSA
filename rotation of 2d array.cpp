60c785f58e173813e1832065923c16a03df29d7f
Keshav <kk2556583@gmail.com>
2026-02-16 01:39:21 +0530

    using constant time without any extra space leet code - 48

--- rotation of 2d array.cpp
+++ rotation of 2d array.cpp
@@ -1,12 +1,46 @@
 #include<iostream>
 using namespace std ;
+/* leetcode -48 Rotate image 
+1. using extra space  - S.C O(n)
+2. using transpose and reverse it 
+S.c - O(1)
 
+
+
+*/
 int main (){
 	int arr [4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-	int mat[4][4];
-	int matrix[4][4];
-	int col =4;
- // using extra space O(n)
+	// using transpose and reverse 
+	// transpose 
+	for (int i = 0;i<4-1;i++){
+		for (int j =i+1;j<4;j++){
+			swap(arr[i][j],arr[j][i]);
+		}
+	}
+	// reverse the row 
+	
+	for(int i =0;i<4;i++){
+		int start =0;
+	int end =3;
+		while (start<end ){
+			swap (arr[i][start],arr[i][end]);
+			start ++;
+			end--;
+		}
+	}cout <<" "; 
+	for(int i =0;i<4;i++){
+		for (int j =0;j<4;j++){
+		cout<<arr[i][j]<<" ";
+		}
+		cout<<"\n ";
+	
+	}
+	
+	//int mat[4][4];
+	// int matrix[4][4];
+	
+//	int col =4;
+ /*// using extra space O(n)
 	// clock wise rotation 
 	for(int i =0;i<4;i++){
 		for (int j =0;j<4;j++){
@@ -42,7 +76,7 @@ int main (){
 	
 	}
 	
-	
+	*/
 
 	return 0 ;
 	
