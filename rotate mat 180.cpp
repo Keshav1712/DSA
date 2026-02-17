5a6daad003b02597e465f6a6daefad5b37a12521
Keshav <kk2556583@gmail.com>
2026-02-18 00:25:38 +0530

    Rotate matrix 180 degree

---
+++ rotate mat 180.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	 int arr [3][3]={1,2,3,4,5,6,7,8,9};
+	 int n =3;
+	 // reverse column
+	 
+	 for(int j =0;j<3;j++){
+	 	int start =0;
+	 	int end = n-1 ;
+	 	while(start<end){
+	 		swap(arr[start][j],arr[end][j]);
+	 		start ++;
+	 		end--;
+		 }
+	 }
+	 // reverse row 
+ for(int i =0;i<3;i++){
+	 	int start =0;
+	 	int end = n-1 ;
+	 	while(start<end){
+	 		swap(arr[i][start],arr[i][end]);
+	 		start ++;
+	 		end--;
+		 }
+	 }
+// display 
+for(int i =0;i<3;i++){
+	for(int j=0;j<3;j++){
+		cout <<arr[i][j]<<"  ";
+	}
+	cout<<endl;
+}
+
+
+	return 0;
+}
