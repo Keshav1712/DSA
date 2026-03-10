e83f12c96a0d2780197f4110a7d0f6b8ef3f54f0
Keshav <kk2556583@gmail.com>
2026-03-10 23:26:35 +0530

    concatenation in string

--- String.cpp
+++ String.cpp
@@ -5,18 +5,19 @@ using namespace std;
 int main (){
 	
 	// Creating String 
-	string s ;
-	cin>>s;
-	cout<<s;
+	string s = "Keshav";
+
+	string str2 = "Kumar";
+	string result1 = s + str2;
+    cout << "Concatenation using + : " << result1 << endl;
 //	cout << "Character at index 6: " << s.at(4) << endl;
-	cout << "Using index: ";
-    for (int i = 0; i < s.size(); i++) {
-        cout <<s[i]<<endl;
-    }
-     cout << "Using range-based for loop: ";
-    for (char ch :s) {
-        cout << ch ;
-    }
-    cout << endl;
-	return 0 ;
+//	cout << "Using index: ";
+//    for (int i = 0; i < s.size(); i++) {
+//        cout <<s[i]<<endl;
+    
+//     cout << "Using range-based for loop: ";
+//    for (char ch :s) {
+//        cout << ch ;
+//    }
+    return 0 ;
 }
