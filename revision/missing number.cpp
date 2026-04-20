9457705c6c7112eb750b1d0eb618a10a67342710
Keshav <kk2556583@gmail.com>
2026-04-20 21:30:32 +0530

    revision part

---
+++ revision/missing number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int arr[]={1,2,4,5};
+	// missing number in a sorted array 
+	int n = sizeof(arr)/sizeof(arr[0]);
+	int m = arr[n-1];
+	int sum1 = (m*((m+1)/2));
+	int sum =0;
+	for(int i =0;i<n;i++){
+		sum +=arr[i];
+		
+	}
+	
+	cout<<sum1-sum;
+	
+	return 0;
+}
